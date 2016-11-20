383e80b2ef5376702e23d8926b7f23e733ffecfe
magwalasek@gmail.com <Magdalena.Walasek@eu.jll.com>
2016-11-20 19:31:26 +0100

    add js task

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
+var animal = 'Zielone słonie';
+text = text.replace('Papugi', animal.toUpperCase());
+console.log(text.slice(0,text.length/2));
